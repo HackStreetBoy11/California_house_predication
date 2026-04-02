5f87be3d287daf81b18e600aceaa311d57844786
varun sammal <varunsammal90@gmail.com>
2026-04-02 17:07:42 +0530

    home page updated

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def predict():
     data=[float(x) for x in request.form.values()]
     final_input = scaler.transform(np.array(data).reshape(1,-1))
     print(final_input)
-    output = model.predict(final_input)[0]
+    output = model.predict(final_input)[0] * 100000
+    output = "{:.2f}".format(output)
     return render_template('home.html',prediction_text='The House price predicted is {}'.format(output))
 
 
